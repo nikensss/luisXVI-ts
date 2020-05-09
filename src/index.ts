718cd411ad0ec1e416e2c70d05407a9d0a5e74e3
nikensss <rikitzzz@gmail.com>
2020-05-09 13:24:11 +0200

    code beautification

--- src/index.ts
+++ src/index.ts
@@ -5,7 +5,9 @@ import Telegram from './utils/Telegram';
 (async () => {
   const luisXVI: LuisXVI = new LuisXVI();
   await luisXVI.fetchData(6);
+
   const result = await luisXVI.crunch('likes');
+
   const richard: Feynmann = new Feynmann();
   richard.exportToHtml(result);
   Telegram.getInstance().stopPolling();
