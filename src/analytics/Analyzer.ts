cbe52e8118527a75a66477f41907d16ab8a3c268
nikensss <rikitzzz@gmail.com>
2020-05-13 19:10:08 +0200

    added and deleted files

---
+++ src/analytics/Analyzer.ts
@@ -0,0 +1,30 @@
+import { default as groupby } from 'group-array';
+import PeriodAggregations from './PeriodAggregations';
+import PeriodTweets from './PeriodTweets';
+import Period from './enums/Period';
+import Tweet from './Tweet';
+
+/**
+ * The Analyzer class is responsible for the mathematic
+ * aggregations over each tweet metric.
+ */
+class Analyzer {
+  private _tweets: Tweet[];
+  constructor(tweets: Tweet[]) {
+    this._tweets = tweets;
+  }
+
+  // public rate(propA: string, propB: string): PeriodAggregation {
+  //   const sumPropA: PeriodAggregation = this.sum(propA);
+  //   const sumPropB: PeriodAggregation = this.sum(propB);
+
+  //   return <PeriodAggregation>this.diveRate(sumPropA, sumPropB);
+  // }
+
+  public sum(metrics: string[], periods: Period[]): PeriodAggregations[] {
+    const periodsTweets: PeriodTweets[] = PeriodTweets.fromAny(groupby(this._tweets, ...periods), periods);
+    return periodsTweets.map((periodTweets) => periodTweets.sum(metrics));
+  }
+}
+
+export default Analyzer;
---
+++ src/datavisualization/Visualizer.ts
@@ -0,0 +1,35 @@
+import fs from 'fs';
+import path from 'path';
+import PeriodAggregations from '../analytics/PeriodAggregations';
+import nunjucks, { Environment } from 'nunjucks';
+import ora, { Ora } from 'ora';
+import TableBuilder from './TableBuilder';
+
+/**
+ * The Visualizer helps visualizing all the data in neat HTML tables.
+ * It should also be able to generate a PDF out of the HTML output.
+ */
+class Visualizer {
+  private njk: Environment;
+  constructor() {
+    this.njk = nunjucks.configure(path.join(__dirname, 'views'), { autoescape: false });
+  }
+
+  exportToHtml(periods: PeriodAggregations[]) {
+    const spinner: Ora = ora({ text: 'exporting to HTML', prefixText: '[Feynmann]' }).start();
+    const tableBody = new TableBuilder(periods);
+    const table = `<table class="rounded m-3 bg-white">\n${tableBody.build('Metrics')}</table>`;
+    const html = this.njk.render('report.njk', { table });
+
+    fs.writeFileSync(path.join(__dirname, 'reports', 'report.html'), html);
+    spinner.succeed('Successfully exported to HTML!');
+  }
+
+  //Private implementations
+
+  log(msg: string): void {
+    console.log(`[Feynmann] ${msg}`);
+  }
+}
+
+export default Visualizer;
